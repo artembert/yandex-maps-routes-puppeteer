0f76c74cebc18cd8dc136cc481656a9a6b281d64
Artem Beresnev <artembert@gmail.com>
2020-05-10 23:09:31 +0300

    Add date to output files

--- src/file-paths.ts
+++ src/file-paths.ts
@@ -1,5 +1,27 @@
+import DateTimeFormatOptions = Intl.DateTimeFormatOptions;
+
+const formatOptions: DateTimeFormatOptions = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  hour12: false,
+};
+
+const startTime = formatDate(new Date(Date.now()));
+
+export function formatDate(date: Date): string {
+  const intlDate = new Intl.DateTimeFormat("default", formatOptions).format(date);
+  return intlDate
+    .replace(/:/g, `.`)
+    .replace(/\//g, `.`)
+    .replace(/, /g, `-`);
+}
+
 export const filePaths = {
-  routesPartials: "../data/routes/routes-partials.json",
-  routesCommon: "../data/routes/routes-common.json",
+  routesPartials: `../data/routes/routes-partials-${startTime}.json`,
+  routesCommon: `../data/routes/routes-common-${startTime}.json`,
   departureCoordinates: "../data/departure-coordinates.json",
 };
