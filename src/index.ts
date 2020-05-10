509adb4d1aae4214e4b182847efb741e510c99a7
Artem Beresnev <artembert@gmail.com>
2020-05-10 16:25:45 +0300

    Open Route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import * as puppeteer from "puppeteer";
 
+const [moscowCenterLon, moscowCenterLat] = [55.748914, 37.612586];
+
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://example.com");
+  await page.goto(getQueryParams([56.024122, 36.587356]));
 })();
+
+function getQueryParams([departureLon, departureLat]: [number, number]): string {
+  return (
+    "https://yandex.ru/maps/?ll=" +
+    moscowCenterLat +
+    "%2C" +
+    moscowCenterLon +
+    "&mode=routes" +
+    "&routes%5Bavoid%5D=tolls" +
+    "&routes%5BtimeDependent%5D%5Btime%5D=2019-12-12T08%3A00%3A00" +
+    "&rtext=" +
+    departureLon +
+    "%2C" +
+    departureLat +
+    "~" +
+    moscowCenterLon +
+    "%2C" +
+    moscowCenterLat +
+    "&rtn=1" +
+    "&rtt=auto" +
+    "&ruri=~&z=9"
+  );
+}
