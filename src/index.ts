0efedd9345916eba302bcd1fb30aefba2f3de254
Artem Beresnev <artembert@gmail.com>
2020-05-10 16:09:32 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+})();
