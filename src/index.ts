d09ac00809f8b9dea548766ec23be7195a3cb99a
Artem Beresnev <artembert@gmail.com>
2020-05-10 18:14:43 +0300

    Print route info to console

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import * as puppeteer from "puppeteer";
+import { LaunchOptions, Page, Response } from "puppeteer";
 
 const [moscowCenterLon, moscowCenterLat] = [55.748914, 37.612586];
+const browserParams: LaunchOptions = {
+  headless: false,
+  defaultViewport: {
+    width: 1024,
+    height: 768,
+  },
+  devtools: true,
+  args: [`--window-size=1578,891`, `--window-position=460,222`],
+};
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch(browserParams);
   const page = await browser.newPage();
+  await page.goto("https://yandex.ru/maps/?ll=37.578087%2C55.723876&z=9");
   await page.goto(getQueryParams([56.024122, 36.587356]));
+  await changeRouteDirection(page);
+  page.on("response", handleResponse);
+  await changeRouteDirection(page);
 })();
 
 function getQueryParams([departureLon, departureLat]: [number, number]): string {
@@ -30,3 +44,27 @@ function getQueryParams([departureLon, departureLat]: [number, number]): string 
     "&ruri=~&z=9"
   );
 }
+
+async function handleResponse(response: Response): Promise<RouteParams | false> {
+  const req = response.request();
+  if (!req.url().includes("buildRoute?")) {
+    return false;
+  }
+  try {
+    const body = (await response.json()) as any;
+    const route = { ...body.data.routes[0] };
+    delete route.encodedCoordinates;
+    delete route.paths;
+    delete route.bounds;
+    console.log(JSON.stringify(route, undefined, 4));
+    return route as RouteParams;
+  } catch (e) {
+    console.error("Cant parse response\n", e);
+  }
+}
+
+async function changeRouteDirection(page: Page): Promise<void> {
+  await page.click(".route-form-view__reverse-icon");
+}
+
+export type RouteParams = any;
